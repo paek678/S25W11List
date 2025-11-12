12bd25cc9312f50b9cfbe72e13b8115eefe1056e
백성열 <paek678@naver.com>
2025-11-11 20:59:35 -0800

    Initial Commit

---
+++ S25W11List/S25W11ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  S25W11ListApp.swift
+//  S25W11List
+//
+//  Created by Student on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct S25W11ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ S25W11ListTests/S25W11ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  S25W11ListTests.swift
+//  S25W11ListTests
+//
+//  Created by Student on 11/11/25.
+//
+
+import Testing
+@testable import S25W11List
+
+struct S25W11ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
